66bdd9c89e63051eb92e1c327afc22be819e26c5
David Lopez <adavidlopezmathez@gmail.com>
2025-01-20 13:22:57 -0300

    Proyecto termiando

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Declaramos el array
+let amigos = [];
+
+//Llamamos a los elementos del HTML
+let campoNombre = document.getElementById("amigo");
+let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+
+//Función para agregar amigos
+let agregarAmigo = (e) => {    
+e.preventDefault();
+let nombreAmigo = campoNombre.value
+
+if(nombreAmigo === ""){
+    alert("Por favor, inserte un nombre");
+    return;
+} else{
+    amigos.push(nombreAmigo);
+    campoNombre.value = "";   
+}
+mostrarAmigos();
+};
+
+//Función para mostrar amigos
+let mostrarAmigos = () => {
+    listaAmigos.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+    
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    
+    }
+};
+
+//Función para sortear amigo
+sortearAmigo = () => {
+let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+resultado.innerHTML = amigos[numeroAleatorio];
+
+
+}
+
+
